b044cfd7c42fddc61131e0d518dca26e0cbd4c8b
arthurviegasr <arthurviegasr@gmail.com>
2021-10-28 02:40:00 -0300

    Tabela de multiplicacao

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,11 @@
-function nomeDaFuncao() { 
-    // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
-    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
+function tableMult() { 
     let x = [];
     for (let i = 0; i <= 10; i++) {
         x[i] = [];
+        for (let j = 0; j <= 10; j++) {
+            x[i][j] = i*j;
+        }
     }
     console.table(x)
 }
+tableMult();
